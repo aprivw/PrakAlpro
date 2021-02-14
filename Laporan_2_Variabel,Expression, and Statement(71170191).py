3269555e78ddad7434aab7a6b9c453d007f29bbf
aprianti vivison w <54616665+aprivw@users.noreply.github.com>
2021-02-14 18:28:50 +0700

    Create Laporan_2_Variabel,Expression, and Statement(71170191).py
    Laporan praktikum 2 -Variabel, expressions and statements
    
    Aprianti Vivison Wijaya -71170191

---
+++ Laporan_2_Variabel,Expression, and Statement(71170191).py
@@ -0,0 +1,19 @@
+#Aprianti Vivison Wijaya
+#71170191
+#Universitas kristen duta wacana
+
+#Program akan menerima 4 inputan
+
+print("==Kasir sederhana==")
+beli1 = float(input("Harga barang pertama: "))
+beli2 = float(input("Harga barang kedua: "))
+beli3 = float(input("Harga barang ketiga: "))
+uang = float(input("Uang yang dibayarkan: "))
+hit =beli1 + beli2 +beli3
+jml = uang - hit
+print("Total yang dibayarkan: ",hit)
+print("Jumlah uang yang dibayarkan: ",uang)
+print("Kembalian: ",jml)
+
+# -
+# +
