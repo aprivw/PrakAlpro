37468e0972abeb3ab931b76c3fee644e1c9a2f72
aprianti vivison w <54616665+aprivw@users.noreply.github.com>
2021-02-23 11:50:54 +0700

    Create Laporan_3_StrukturKontrolPercabangan(71170191).py

---
+++ Laporan_3_StrukturKontrolPercabangan(71170191).py
@@ -0,0 +1,56 @@
+#Aprianti Vivison Wijaya
+#71170191
+#UKDW
+#Struktur Kontrol Percabangan
+
+print("Selamat datang di toko buah")
+print("1.Pilih buah")
+print("2.Exit")
+harga = 0
+uang = 0
+pil = input("Masukkan pilihan : ")
+if pil == "1":
+    apel = 13000
+    kiwi = 15000
+    semangka = 20000
+    print ("1.Apel 13.000/kg")
+    print ("2.Kiwi 15.000/kg")
+    print ("3.Semangka 20000/kg")
+    pil_buah = input("Masukkan pilihan: ")
+    if pil_buah == "1":
+        jml_buah =int(input("Masukkan berapa kg yang akan dibeli: "))
+        uang = float(input("Masukkan uang anda: "))
+        harga = jml_buah * apel
+        total = uang - harga
+        print("================================================")
+        print("Anda membeli ",jml_buah," kg apel seharga ",harga)
+        print("Jumlah uang anda : ",uang)
+        print("Kembalian anda : ",total)
+        print("================================================")
+
+    elif pil_buah == "2":
+        jml_buah =int(input("Masukkan berapa kg yang akan dibeli: "))
+        uang = float(input("Masukkan uang anda: "))
+        harga = jml_buah * kiwi
+        total = uang - harga
+        print("================================================")
+        print("Anda membeli ",jml_buah," kg kiwi seharga ",harga)
+        print("Jumlah uang anda : ",uang)
+        print("Kembalian anda : ",total)
+        print("================================================")
+    elif pil_buah == "3":
+        jml_buah =int(input("Masukkan berapa kg yang akan dibeli: "))
+        uang = float(input("Masukkan uang anda: "))
+        harga = jml_buah * semangka
+        total = uang - harga
+        print("====================================================")
+        print("Anda membeli ",jml_buah," kg semangka seharga ",harga)
+        print("Jumlah uang anda : ",uang)
+        print("Kembalian anda : ",total)
+        print("====================================================")
+    else:
+        print("Pilihan tidak ada")
+elif pil =="2":
+    print("Keluar dari program")
+else:
+    print("Tidak ada pilihan tersebut")
