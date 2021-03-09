940b34e407609a79a8e7b4a7326cb93b04adc54e
aprianti vivison w <54616665+aprivw@users.noreply.github.com>
2021-03-09 22:14:14 +0700

    Create Laporan_5_StrukturKontrolPerulangan.py

---
+++ Laporan_5_StrukturKontrolPerulangan.py
@@ -0,0 +1,11 @@
+awal = int(input("Masukkan saldo awal: "))
+sisa = awal
+while (sisa > 0):
+    pengeluaran = int(input("Masukkan pengeluaran: "))
+    if pengeluaran == -1:
+        print("sisa saldo", sisa)
+    elif pengeluaran >-1:
+        sisa = sisa - pengeluaran
+        print("sisa saldo: ", sisa)
+print("saldo habis gan, berhenti belanja")
+    
