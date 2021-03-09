5936cf23e8d62a128d00aa1bd94da9c130b24b4f
aprivw <54616665+aprivw@users.noreply.github.com>
2021-03-09 23:38:12 +0700

    Update Laporan_5_StrukturKontrolPerulangan.py

--- Laporan_5_StrukturKontrolPerulangan.py
+++ Laporan_5_StrukturKontrolPerulangan.py
@@ -4,10 +4,15 @@ awal = int(input("Masukkan saldo awal: "))
 sisa = awal
 while (sisa > 0):
     pengeluaran = int(input("Masukkan pengeluaran: "))
-    if pengeluaran == -1:
-        print("sisa saldo", sisa)
+    if pengeluaran <= -1: 
+        print("Sisa saldo", sisa,"\nPengeluaran tidak boleh negatif")
+        break
     elif pengeluaran >-1:
-        sisa = sisa - pengeluaran
-        print("sisa saldo: ", sisa)
-print("saldo habis gan, berhenti belanja")
-    
+        sisa  = sisa - pengeluaran
+        print("sisa saldo: ",sisa)
+if awal==0:
+    print("Saldo awal masih 0, tambah gan")
+elif sisa == 0:
+    print("Saldo habis gan, berhenti belanja")
+elif sisa > 0:
+    print("Masih ada saldo gan, belanja lagi")
